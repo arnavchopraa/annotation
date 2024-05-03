09a023a248e5dc90bcca27b8ef24aecb35224bad
pauls <P.G.Stan@student.tudelft.nl>
2024-05-03 15:35:09 +0200

    Added functionality to connect to a MySQL db and fetch the contents of the annotations.

--- Annotation_Tool/src/main/java/org/example/Main.java
+++ Annotation_Tool/src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // url is of the form: "jdbc:subprotocolName:subnameURL" last name is for the schema of the db
+        String dbURL = "jdbc:mysql://localhost:3306/annotation";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(dbURL, "root", "Universitate!1");
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("select * from annotations");
+
+            while(resultSet.next()) {
+                System.out.println(resultSet.getString("codeContent"));
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
