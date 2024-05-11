745d13bd5de8c719ac8d4fce1f589b1c3167beb5
Rebecca Andrei <n.r.andrei@student.tudelft.nl>
2024-05-11 18:47:59 +0200

    added javadoc comments for java files

--- Annotation_Tool/src/main/java/org/example/utils/PairUtils.java
+++ Annotation_Tool/src/main/java/org/example/utils/PairUtils.java
@@ -9,18 +9,30 @@ public class PairUtils {
         this.annotations = annotations;
     }
 
+    /**
+     * Getter for the text within the PDF.
+     */
     public String getText() {
         return text;
     }
 
+    /**
+     * Getter for the annotations within the PDF.
+     */
     public String getAnnotations() {
         return annotations;
     }
 
+    /**
+     * Setter for the text within the PDF.
+     */
     public void setText(String text) {
         this.text = text;
     }
 
+    /**
+     * Setter for the annotations within the PDF.
+     */
     public void setAnnotations(String annotations) {
         this.annotations = annotations;
     }
