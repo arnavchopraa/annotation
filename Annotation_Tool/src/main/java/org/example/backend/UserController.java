57a671257353b0e0ae4a5d19d0960c6039d54f37
Calin Georgescu <georgescu-2@student.tudelft.nl>
2024-05-22 13:49:23 +0300

    Bugfix

--- Annotation_Tool/src/main/java/org/example/backend/UserController.java
+++ Annotation_Tool/src/main/java/org/example/backend/UserController.java
@@ -1,7 +1,7 @@
 package org.example.backend;
 
 import org.example.models.User;
-import org.example.repositories.UserRepository;
+import org.example.database.UserRepository;
 import org.example.database.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,18 +16,35 @@ public class UserController {
 	private UserRepository repo;
 	private UserService service;
 
+	/**
+	 * Constructor for the UserController
+	 *
+	 * @param repo the repository for the user
+	 * @param service the service for the user
+	 */
 	@Autowired
 	public UserController(UserRepository repo, UserService service) {
 		this.repo = repo;
 		this.service = service;
 	}
 
+	/**
+	 * This method returns all the users in the database
+	 *
+	 * @return a list of all the users in the database
+	 */
 	@GetMapping("/")
 	@ResponseBody
 	public List<User> getUsers() {
 		return repo.findAll();
 	}
 
+	/**
+	 * This method returns the user with the given id
+	 *
+	 * @param id the id of the user
+	 * @return the user with the given id
+	 */
     @GetMapping("/{id}")
  	@ResponseBody
 	public ResponseEntity<User> getUser( @PathVariable("id") long id) {
@@ -38,6 +55,12 @@ public class UserController {
 		return ResponseEntity.ok(user);
 	}
 
+	/**
+	 * This method adds a user to the database
+	 *
+	 * @param user the user to be added
+	 * @return the user that was added
+	 */
 	@PostMapping("/{id}")
 	@ResponseBody
 	public ResponseEntity<User> addUser(@RequestBody User user) {
@@ -48,6 +71,28 @@ public class UserController {
 		return ResponseEntity.ok(user1);
 	}
 
+	/**
+	 * This method updates a user in the database
+	 *
+	 * @param user the user to be updated
+	 * @return the user that was updated
+	 */
+	@PutMapping("/{id}")
+	@ResponseBody
+	public ResponseEntity<User> updateUser(@RequestBody User user) {
+		User user1 = service.updateUser(user);
+		if (user1 == null) {
+			return ResponseEntity.badRequest().build();
+		}
+		return ResponseEntity.ok(user1);
+	}
+
+	/**
+	 * This method deletes a user from the database
+	 *
+	 * @param id the id of the user to be deleted
+	 * @return the user that was deleted
+	 */
 	@DeleteMapping("/{id}")
 	@ResponseBody
 	public void deleteUser(@PathVariable Long id) {
--- Annotation_Tool/src/main/java/org/example/models/User.java
+++ Annotation_Tool/src/main/java/org/example/models/User.java
@@ -1,7 +1,12 @@
 package org.example.models;
 
-import javax.persistance.*;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name="Users")
