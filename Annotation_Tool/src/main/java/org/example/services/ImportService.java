1c40c933175aa21185bcad1eb60bbde58ca3a724
Radu_Andrei_Vasile <r.a.vasile-1@student.tudelft.nl>
2024-05-28 17:47:43 +0200

    saving progress

--- Annotation_Tool/src/main/java/org/example/services/ImportService.java
+++ Annotation_Tool/src/main/java/org/example/services/ImportService.java
@@ -5,10 +5,7 @@ import org.example.exceptions.NoSubmissionException;
 import org.example.models.Student;
 import org.example.models.Submission;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.*;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipException;
@@ -17,6 +14,14 @@ import java.util.zip.ZipFile;
 @NoArgsConstructor
 public class ImportService {
 
+    /**
+     * Import data as downloaded from Brightspace to the application
+     *
+     * @param zipFile .zip file containing student submissions
+     * @param csvFile .csv file containing a list of all students
+     * @throws NoSubmissionException if a directory does not contain any submissions
+     * @throws ZipException if the zip file behaves unexpectedly
+     */
     public void importData(File zipFile, File csvFile) throws NoSubmissionException, ZipException {
         List<Student> studentList = processCsv(csvFile);
         List<Submission> submissionList = extractZip(zipFile);
@@ -24,22 +29,40 @@ public class ImportService {
         //TODO - creez modelul pt baza de date studentNo - fisier - assigned coordinator
     }
 
+    public void associate(List<Student> students, List<Submission> submissions) {
+
+    }
+
+    /**
+     * Extracts data from the zip file containing student submissions, retrieved from Brightspace.
+     *
+     * @param zipFile .zip file containing student submissions
+     * @return List of Submission objects, containing submission data
+     * @throws NoSubmissionException if a directory does not contain any submissions
+     * @throws ZipException if the zip file behaves unexpectedly
+     */
     private List<Submission> extractZip(File zipFile) throws NoSubmissionException, ZipException {
         List<Submission> submissionList = new ArrayList<>();
         try (ZipFile file = new ZipFile(zipFile)) {
             Enumeration<? extends ZipEntry> entries = file.entries();
             while(entries.hasMoreElements()) {
                 ZipEntry entry = entries.nextElement();
-                InputStream inputStream = file.getInputStream(entry);
-                File currentEntry = new File(System.getProperty("java.io.tmpdir") + "/" + entry.getName());
-                if(currentEntry.isDirectory()) {
-                    String name = currentEntry.getName();
+                if(entry.isDirectory()) {
+                    String name = entry.getName();
+
+                    InputStream inputStream = file.getInputStream(entry);
+                    File currentEntry = new File(System.getProperty("java.io.tmpdir") + "/" + entry.getName());
+                    OutputStream outputStream = new FileOutputStream(currentEntry);
+                    outputStream.write(inputStream.readAllBytes());
+
+                    inputStream.close();
+                    outputStream.close();
+
                     Submission submission = processFileName(name);
                     File submittedFile = Objects.requireNonNull(currentEntry.listFiles())[0];
                     submission.setSubmittedFile(submittedFile);
                     submissionList.add(submission);
                 }  //TODO else - for now, should be ignored. Later, should also support files with nested zips / containing csv file.
-                inputStream.close();
             }
         } catch (IOException e) {
             throw new ZipException("Zip file could not be read.");
@@ -51,6 +74,12 @@ public class ImportService {
         return submissionList;
     }
 
+    /**
+     * Extracts data from a csv file containing a list of all students, retrieved from Brightspace
+     *
+     * @param csvFile .csv file containing a list of all students
+     * @return List of Student objects, containing student data
+     */
     private List<Student> processCsv(File csvFile) {
         List<Student> students = new ArrayList<>();
         try (Scanner scanner = new Scanner(csvFile)) {
@@ -74,6 +103,13 @@ public class ImportService {
         return students;
     }
 
+    /**
+     * Processes a submission directory name, parsing all data according to the format
+     * defined by the Brightspace export tool.
+     *
+     * @param name The name of a directory containing student submissions
+     * @return a Submission object, containing all data
+     */
     private Submission processFileName(String name) {
         String[] split = name.split("-");
         String groupNo = split[0];
