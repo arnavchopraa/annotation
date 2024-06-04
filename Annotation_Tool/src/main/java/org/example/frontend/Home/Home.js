9b0e24d4c53505c8a4b948168c7be99eb081e686
Calin Georgescu <georgescu-2@student.tudelft.nl>
2024-06-04 14:21:22 +0200

    Revert "Merge branch '15-backend-functionality-for-new-frontend' into 'dev'"
    This reverts merge request !32

--- Annotation_Tool/src/main/java/org/example/frontend/Home/Home.js
+++ Annotation_Tool/src/main/java/org/example/frontend/Home/Home.js
@@ -3,15 +3,107 @@ const pdfObject = document.getElementById('pdfObject');
 let getName = localStorage.getItem('file')
 /**
     * Detect when a file has been inputted and render the PDF file.
+    * Also listen for changes in the file such as adding / editing / deleting annotations and saving the file
 **/
 fileInput.addEventListener('change', function(e) {
-    var file = e.target.files[0];
-    if (!file)
-        return;
+        var file = e.target.files[0];
+        if (!file)
+            return;
 
-    // render the PDF file
-    pdfObject.src = URL.createObjectURL(file);
-});
+        //showcasing the file
+		var adobeDCView = new AdobeDC.View({clientId: "543b9355cff44d19821857d8b0ddfb96", divId: "pdfContainer"});
+		adobeDCView.previewFile({
+			content:{location: {url: URL.createObjectURL(file)}},
+			metaData:{fileName: file.name, id: file.name}
+		},
+		{
+            enableAnnotationAPIs: true,
+            includePDFAnnotations: true
+        });
+
+        //options for the saving of the document
+        const saveOptions = {
+           autoSaveFrequency: 0,
+           enableFocusPolling: false,
+           showSaveButton: true
+        }
+
+        //registering the save functionality
+        adobeDCView.registerCallback(
+           AdobeDC.View.Enum.CallbackType.SAVE_API,
+           function(metaData, content, options) {
+              console.log("Document was saved")
+              return new Promise((resolve, reject) => {
+                 resolve({
+                    code: AdobeDC.View.Enum.ApiResponseCode.SUCCESS,
+                    data: {
+                      metaData: Object.assign(metaData, {fileName: file.name})
+                    }
+                 });
+              });
+           },
+        saveOptions);
+
+
+        //promise used to listen to changes
+        const previewFilePromise = adobeDCView.previewFile({
+                                   			content:{location: {url: URL.createObjectURL(file)}},
+                                   			metaData:{fileName: file.name, id: file.name}
+                                   		},
+                                   		{
+                                               enableAnnotationAPIs: true,
+                                               includePDFAnnotations: true
+                                           });
+
+        //changes that we listen to
+        const eventOptions = {
+        	listenOn: [
+        		"ANNOTATION_ADDED", "ANNOTATION_UPDATED", "ANNOTATION_DELETED"
+        	]
+        }
+
+        //listening to changes in the pdf document
+        previewFilePromise
+        	    .then((adobeViewer) => {
+        			    adobeViewer.getAnnotationManager()
+        						.then(annotationManager => {
+        						    //getting all of the annotations
+        							annotationManager.getAnnotations()
+        								.then(result => {
+        									console.log("All annotations: ", result);
+        								})
+        								.catch(e => {
+        									console.log(e);
+        								});
+
+                                    annotationManager.registerEventListener(
+                                    		function (event) {
+                                    				console.log(event.type, event.data)
+                                    				if (event.type === 'ANNOTATION_ADDED') {
+                                    					    console.log("Annotation added\nAll annotations: ", annotationManager.getAnnotations())
+                                    				} else if (event.type === 'ANNOTATION_UPDATED') {
+                                    						console.log("Annotation updated\nAll annotations: ", annotationManager.getAnnotations())
+                                    				} else if (event.type === 'ANNOTATION_DELETED') {
+                                    						console.log("Annotation deleted\nAll annotations: ", annotationManager.getAnnotations())
+                                    				}
+                                    		},
+                                    		eventOptions
+                                    );
+
+        						})
+        						.catch(e => {
+        							console.log(e);
+        						});
+        		})
+        		.catch(e => {
+        			console.log(e);
+        		});
+
+
+
+
+	});
+
 
 /**
     * Fetch the codes from the database when the page loads.
