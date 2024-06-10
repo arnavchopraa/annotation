d7518bdecb271d3323aa619891c320e46a311048
Rebecca Andrei <n.r.andrei@student.tudelft.nl>
2024-06-10 20:42:35 +0200

    js refractor

--- Annotation_Tool/src/main/java/org/example/frontend/Support/Support.js
+++ Annotation_Tool/src/main/java/org/example/frontend/Support/Support.js
@@ -1,34 +1,34 @@
+const slides = document.querySelectorAll('.slide');
+const options = document.querySelectorAll('.option');
+const backButton = document.querySelector('.back-button');
+
 document.addEventListener('DOMContentLoaded', function() {
-    const slides = document.querySelectorAll('.slide');
-    const options = document.querySelectorAll('.option');
-    const backButton = document.querySelector('.back-button');
-
-    function showSlide(index) {
-        slides.forEach((slide, i) => {
-            if (i === index) {
-                slide.classList.add('active');
-            } else {
-                slide.classList.remove('active');
-            }
-
-            if (index === 0) {
-                backButton.style.display = 'none';
-            } else {
-                backButton.style.display = 'flex';
-            }
-        });
-    }
-
     options.forEach((option, index) => {
         option.addEventListener('click', () => {
             showSlide(index + 1);
         });
     });
 
-    backButton.addEventListener('click', () => {
-        showSlide(0);
-    });
+    openSlide();
+});
 
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        if (i === index) {
+            slide.classList.add('active');
+        } else {
+            slide.classList.remove('active');
+        }
+
+        if (index === 0) {
+            backButton.style.display = 'none';
+        } else {
+            backButton.style.display = 'flex';
+        }
+    });
+}
+
+function openSlide() {
     const urlParams = new URLSearchParams(window.location.search);
     if (urlParams.get('slide') === 'contact') {
         showSlide(1);
@@ -39,4 +39,8 @@ document.addEventListener('DOMContentLoaded', function() {
     } else {
         showSlide(0);
     }
+}
+
+backButton.addEventListener('click', () => {
+    showSlide(0);
 });
