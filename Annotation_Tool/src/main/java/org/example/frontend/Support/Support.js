12828f4698463a8136d2fc70cc9dbf45417e4cf3
Rebecca Andrei <n.r.andrei@student.tudelft.nl>
2024-06-10 22:39:39 +0200

    feedback form

--- Annotation_Tool/src/main/java/org/example/frontend/Support/Support.js
+++ Annotation_Tool/src/main/java/org/example/frontend/Support/Support.js
@@ -1,7 +1,11 @@
 const slides = document.querySelectorAll('.slide');
 const options = document.querySelectorAll('.option');
 const backButton = document.querySelector('.back-button');
+const textFields = document.querySelectorAll('.text-field');
 
+/**
+    * @returns {void} adds an event listener to each option and associates them with the corresponding slide
+*/
 document.addEventListener('DOMContentLoaded', function() {
     options.forEach((option, index) => {
         option.addEventListener('click', () => {
@@ -10,8 +14,20 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     openSlide();
+    removePlaceholder();
 });
 
+/**
+    * @returns {void} shows the first slide when the back button is clicked
+*/
+backButton.addEventListener('click', () => {
+    showSlide(0);
+});
+
+/**
+    * @param {number} index - the index of the slide to show
+    * @returns {void} shows the slide with the given index
+*/
 function showSlide(index) {
     slides.forEach((slide, i) => {
         if (i === index) {
@@ -25,22 +41,67 @@ function showSlide(index) {
         } else {
             backButton.style.display = 'flex';
         }
+
+        updateURL(slideName(index));
     });
 }
 
+/**
+    * @returns {void} opens the slide corresponding to the url's query parameter
+*/
 function openSlide() {
     const urlParams = new URLSearchParams(window.location.search);
-    if (urlParams.get('slide') === 'contact') {
-        showSlide(1);
-    } else if (urlParams.get('slide') === 'faq') {
-        showSlide(2);
-    } else if (urlParams.get('slide') === 'feedback') {
-        showSlide(3);
+    showSlide(indexSlide(urlParams.get('slide')));
+}
+
+/**
+    * @param {string} slide - the name of the slide
+    * @returns {void} updates the page's url with the given slide name
+*/
+function updateURL(slide) {
+    const url = new URL(window.location);
+    if (slide) {
+        url.searchParams.set('slide', slide);
     } else {
-        showSlide(0);
+        url.searchParams.delete('slide');
+    }
+    history.pushState(null, '', url);
+}
+
+/**
+     * @param {number} index
+     * @returns {name} name corresponding to the slide with the given index
+ */
+function slideName(index) {
+    switch(index) {
+        case 1: return 'contact';
+        case 2: return 'faq';
+        case 3: return 'feedback';
+        default: return 'support';
     }
 }
 
-backButton.addEventListener('click', () => {
-    showSlide(0);
-});
+/**
+    * @param {string} name
+    * @returns {number} index corresponding to the slide with the given name
+*/
+function indexSlide(name) {
+    switch(name) {
+        case 'contact': return 1;
+        case 'faq': return 2;
+        case 'feedback': return 3;
+        default: return 0;
+    }
+}
+
+function removePlaceholder() {
+    textFields.forEach(function(textField) {
+        textField.addEventListener('input', function() {
+            if (this.textContent.trim() !== '') {
+                this.setAttribute('data-empty', 'false');
+            } else {
+                this.setAttribute('data-empty', 'true');
+            }
+        });
+    });
+}
